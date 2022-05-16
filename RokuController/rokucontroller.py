7f20643ca3cdac94c448b7ae0ff07fe3d01a04ec
levelfourtwenty <level420@protonmail.com>
2022-05-16 13:08:58 -0500

    hopefully fixed the cmdline issues

--- RokuController/rokucontroller.py
+++ RokuController/rokucontroller.py
@@ -1,19 +1,19 @@
 from curtsies import Input
 from sendHttpRequests import requestHandler 
 
+class rokucontroller:
 
-def main():
-	actions = requestHandler.actions
-
-	with Input(keynames='curses') as input_generator:
-		for keypress in input_generator:
-			print(keypress)
-			# Checks to see if keypress is a command or is something that is to be used as user text input
-			if keypress not in actions:
-				requestHandler.sendRequests(keypress, True)
-			else:
-				requestHandler.sendRequests(actions[keypress], False)
+	def main():
+		actions = requestHandler.actions
+		with Input(keynames='curses') as input_generator:
+			for keypress in input_generator:
+				print(keypress)
+				# Checks to see if keypress is a command or is something that is to be used as user text input
+				if keypress not in actions:
+					requestHandler.sendRequests(keypress, True)
+				else:
+					requestHandler.sendRequests(actions[keypress], False)
 					
 	
-if __name__ == '__main__':
-	main()
+if __name__ == '__rokucontroller__':
+	rokucontroller()
