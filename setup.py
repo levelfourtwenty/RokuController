9bbe84148ed86c7f63015f6d299df4f812c08eed
levelfourtwenty <level420@protonmail.com>
2022-05-16 16:59:03 -0500

    trying to fix the long description on pypi

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'RokuController',         
   packages = ['RokuController'],   
-  version = '0.2.5.9.1', 
+  version = '1.1', 
   license='gpl-2.0',        
   description = 'An easy to use Roku cli interface.',   
   author = 'Levelfourtwenty',                
   author_email = 'level420@protonmail.com',     
   url = 'https://github.com/levelfourtwenty/RokuController/',  
-  download_url = 'https://github.com/levelfourtwenty/RokuController/v_0.2.5.9.1.tar.gz',
+  download_url = 'https://github.com/levelfourtwenty/RokuController/v_1.1.tar.gz',\
   keywords = ['Controller', 'Roku Cli', 'Roku'],
   install_requires=[            
           'requests',
@@ -17,9 +17,9 @@ setup(
 
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',     
+    'Development Status :: 5 - Production/Stable',     
     'Intended Audience :: Developers', 
-    'Topic :: Software Development :: Build Tools',
+    'Topic :: Utilities',
     'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',  
     'Programming Language :: Python :: 3',  
     'Programming Language :: Python :: 3.7',
