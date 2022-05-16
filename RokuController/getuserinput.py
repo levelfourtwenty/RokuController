71f214408732c655cfc6324b61b3de78385b9eb9
levelfourtwenty <level420@protonmail.com>
2022-05-16 14:48:53 -0500

    fixed the import shit

--- RokuController/getuserinput.py
+++ RokuController/getuserinput.py
@@ -1,16 +1,16 @@
 from curtsies import Input
-import RokuController
+from RokuController import sendHttpRequests
 
 def main():
-	actions = RokuController.requestHandler.actions
+	actions = sendHttpRequests.requestHandler.actions
 	with Input(keynames='curses') as input_generator:
 		for keypress in input_generator:
 			print(keypress)
 			# Checks to see if keypress is a command or is something that is to be used as user text input
 			if keypress not in actions:
-				RokuController.requestHandler.sendRequests(keypress, True)
+				sendHttpRequests.requestHandler.sendRequests(keypress, True)
 			else:
-				RokuController.requestHandler.sendRequests(actions[keypress], False)
+				sendHttpRequests.requestHandler.sendRequests(actions[keypress], False)
 					
 	
 if __name__ == '__rokucontroller__':
