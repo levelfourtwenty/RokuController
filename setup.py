8b21c7d6f8af4c78598bc364c72065baa5272dd6
levelfourtwenty <level420@protonmail.com>
2022-05-16 12:25:03 -0500

    bruh

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     'Development Status :: 3 - Alpha',     
     'Intended Audience :: Developers', 
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: GNU GPL 2',  
+    'License :: OSI Approved :: GNU GPL v2',  
     'Programming Language :: Python :: 3',  
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
